24867aa1e639cffe0fbfe794a2ba9453cf8a0144
narimenmokrani1 <narimenmokrani@gmail.com>
2022-02-14 11:37:09 -0600

    scaffolfd the about page, installed materialUi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import { useState } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
@@ -6,30 +5,13 @@ import Home from './components/Home/Home';
 import States from './components/navigation/states/States';
 import StateDetails from './components/stateDetails/StateDetails';
 function App() {
-	
- 
-  
-
 	return (
 		<div className='App'>
 			<Routes>
-				<Route
-					path='/'
-					element={<Home  />}
-				/>
-				<Route
-					path='/regions/:region'
-					element={<States  />}
-				/>
+				<Route path='/' element={<Home />} />
+				<Route path='/regions/:region' element={<States />} />
 				{/* <Route path='/about' element={<About />} /> */}
-				<Route
-					path='/states/:_id'
-					element={
-						<StateDetails
-							
-						/>
-					}
-				/>
+				<Route path='/states/:_id' element={<StateDetails />} />
 			</Routes>
 		</div>
 	);
---
+++ src/components/navigation/about/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About(props) {
+	return (
+		<div>
+            <h1>About</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio,
+				dolorum! Laboriosam consequuntur perspiciatis deleniti, enim provident
+				ipsa dolore pariatur corrupti! At ratione quos exercitationem, et a
+				dolor laboriosam odit fugiat.
+			</p>
+		</div>
+	);
+}
+
+export default About;
